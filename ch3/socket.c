b056d26f7537229b8ecd70bc50c85dbdc8c2f8a8
yufenZ <1742945286@qq.com>
2020-05-09 22:13:29 +0800

    TCP套接字

---
+++ ch3/socket.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  socket
+//
+//  Created by yufenZ on 2020/5/9.
+//  Copyright © 2020年 yufenZ. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int make_socket(uint16_t port){
+    int sock;
+    struct sockaddr_in name;
+    
+    //创建字节流类型的IPV4 socket
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock < 0){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    //绑定到port和ip
+    name.sin_family = AF_INET; //IPV4
+    name.sin_port = htons(port); // 指定端口
+    name.sin_addr.s_addr = htonl(INADDR_ANY); //通配地址
+    //把IPV4地址转换成通用地址格式，同时传递长度
+    if(bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0){
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    return sock;
+}
+int main(int argc, const char * argv[]) {
+    int sockfd = make_socket(12345);
+    exit(0);
+}
